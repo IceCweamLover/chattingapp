9383cdd344282b85e53fb208ddc55b984fd283ff
soup <91413092+Siphonerr@users.noreply.github.com>
2021-12-17 22:51:20 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+FORMAT='utf-8'
 PORT = 5050
 import socket
 PORT = 5050
@@ -5,12 +6,15 @@ SERVER = '192.168.15.221'
 ADDR = (SERVER, PORT)
 HEADER=64
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clinet.connect(ADDR)
+client.connect(ADDR)
 def send(msg):
-     message=msg.encode('utf-8')
-     msg_length=len(message)
-     send_length=str(msg_length).encode('utf-8')
-     send_length += b''*(HEADER-len(send_length))
-     client.send(message)
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
 
-
+send("Hello World!")
+input()
