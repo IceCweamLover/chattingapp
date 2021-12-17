c2df6aa5f5a40e9aec970af47915db477fa20fe0
soup <91413092+Siphonerr@users.noreply.github.com>
2021-12-17 20:20:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask import render_template
+import socket #this will be a BIG part of this project
 app=Flask(__name__)
 @app.route('/')
 def mainpage():
-    return render_template("main.html")
+    return render_template("main.html")#uses our html file 
 
 if __name__=='__main__':
-  app.run()
+  app.run()#runs the website when the file is ran
